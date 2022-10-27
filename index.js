879888ec43c34515b4f5dc3914bfb876edfc0eea
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2022-10-27 17:01:34 +0300

    songs folder
    deleted songs folder because now it will create a folder for you.
    deleting all songs inside the songs folder if it exist when the bot is being launched because if the bot crashes then the files will still be there.

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const { Client, Collection } = require("discord.js");
 const client = new Client({ intents: 3276543, partials: ["CHANNEL"] });
 
 const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
+
+if (!fs.existsSync("songs")) {
+  fs.mkdirSync("songs");
+} else {
+  fs.readdir("songs", (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join("songs", file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+}
 
 client.aliases = new Collection();
 client.interactions = new Collection();
